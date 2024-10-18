b1adf1ac16a7d03393cf0fefefc7b18d235f139a
Samantha Cruz <cruz.s98319@gmail.com>
2024-10-17 18:06:28 -0700

    Finished assignment

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -4,11 +4,31 @@ import { useParams } from 'react-router-dom'
 function ArtistView() {
     const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData)
+        }
+        fetchData()
+    }, [id])
+
+    const renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div key=''>
+                <p>{album.collectionName}</p>
+            </div>
+        )
+    })
 
     return (
         <div>
             <h2>The id passed was: {id}</h2>
             <p>Artist Data Goes Here!</p>
+            {renderAlbums}
         </div>
     )
 }
